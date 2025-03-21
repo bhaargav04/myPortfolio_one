7b6c63a2bee7459ae603a681503926e8e437a7a8
Bhaargav <144582477+bhaargav04@users.noreply.github.com>
2025-03-21 22:19:08 +0530

    thirdCommit

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // Allows access from external hosts
-    port: 5173,
+    host: "0.0.0.0",   // Bind to all interfaces
+    port: 5173,        // Change if necessary
     strictPort: true,
-    cors: true
+    cors: true,
+    allowedHosts: ["myportfolio-one.onrender.com"],  // Allow Render domain
   },
   preview: {
     host: "0.0.0.0",
