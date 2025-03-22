16e0df6775f20f66d69466b4401749cc88bcad2d
Bhaargav <144582477+bhaargav04@users.noreply.github.com>
2025-03-22 13:34:01 +0530

    normal

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "../components/Projects/ProjectCard";
 import Particle from "../components/Particle";
-import pg from "../assets/projects/pg.png";
-import project from "../assets/projects/project.jpeg";
-import lift from "../assets/projects/lift.png";
-import krypto from "../assets/projects/krypto.png";
-import kickstart from "../assets/projects/kickstart.png";
+import trafficlights from "../assets/projects/trafficlights.jpg";
+import timetable from "../assets/projects/timetable.jpg";
+import weather from "../assets/projects/weather.png";
+import marvel from "../assets/projects/marvel.jpg";
+import handCricket from "../assets/projects/handCricket.jpg";
+import playstore from "../assets/projects/playstore.png";
 
 const Projects = () => {
   return (
@@ -22,7 +23,7 @@ const Projects = () => {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={lift}
+              imgPath={weather}
               isBlog={false}
               title="Weather Forecast"
               description="This online ride-sharing platform boasts a responsive front-end design, complete with an admin panel built using React and Material-UI. With the integration of Google Maps API, users can easily access directions on the map. The platform allows for full CURD functionality on profiles and posts, and users can confirm or cancel a ride from a post and message each other after confirmation. Additionally, users can easily reset their password. The back-end is powered by a Node Express server and utilizes MongoDB for the database management."
@@ -33,7 +34,7 @@ const Projects = () => {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={project}
+              imgPath={timetable}
               isBlog={false}
               title="Time Table Scheduler"
               description="This platform features a responsive interface built with Reactjs and CSS, allowing users to easily manage their profiles and posts through CRUD activities. The option to follow other users and interact with their posts (like or dislike) is also available, with suggestions for new users to follow. Users can only view posts from their followed users and can only engage in chat with those they follow. The backend is powered by Nodejs and Expressjs server, while MongoDB is utilized as the database."
@@ -44,7 +45,7 @@ const Projects = () => {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={kickstart}
+              imgPath={playstore}
               isBlog={false}
               title="Play Store Replica"
               description="This ecommerce website utilizes a REST API built with Nodejs and Expressjs, allowing for easy CRUD operations on user profiles, products and admin panel. The user interface is designed for efficient product management and includes a complete purchase process. The admin panel, accessible for CRUD on users and products, includes a chart for statistics display. Stripe API is integrated to handle customer payments. The website uses MongoDB and Firebase as the database."
@@ -55,7 +56,7 @@ const Projects = () => {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={pg}
+              imgPath={trafficlights}
               isBlog={false}
               title="Traffic Light"
               description="This blogging application features a RESTful API server built with Nodejs and Expressjs. The data is retrieved from the API server and displayed on a user-friendly interface designed using React, Redux, and styled-components for a seamless experience."
@@ -65,18 +66,18 @@ const Projects = () => {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={krypto}
+              imgPath={marvel}
               isBlog={false}
-              title="Portfolio"
+              title="Webpage using Tailwind"
               description="Project Krypto is a Web 3 project that utilizes an Ethereum smart contract created using Solidity. It allows users to interact with their Metamask wallet and send Ethereum."
-              ghLink="https://github.com/bhaargav04/myPortfolio_one"
-              demoLink="https://project-krypto.netlify.app/"
+              ghLink="https://github.com/bhaargav04/avengers"
+              demoLink="https://bhaargav04.neocities.org/BOOTSTRAP/first"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={kickstart}
+              imgPath={handCricket}
               isBlog={false}
               title="Hand Cricket"
               description="Solidity Kickstart is a Web 3 project that utilizes an Ethereum smart contract created using Solidity. It includes all the functionality of a kickstart platform, enabling users to interact with their Metamask wallet, transfer Ethereum and participate in kickstart campaigns."
